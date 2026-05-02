36ae884f5f5d35dee8aeaf5f951b783fd10b15e4
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-05-02 09:32:39 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import { connectDB } from './src/config/db.js';
 import { ENV } from './src/config/env.js';
 
 const app = express(); 
-
+ 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
   connectDB();
