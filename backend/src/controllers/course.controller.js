bc61ca60cc6be88e02ca66573f64316fb7a103fe
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-05-04 14:53:43 +0530

    change

--- backend/src/controllers/course.controller.js
+++ backend/src/controllers/course.controller.js
@@ -1,4 +1,10 @@
 import cloudinary from "../config/cloudinary.js";
+import { ENV } from "../config/env.js";
+import {Course} from "../models/course.model.js";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAi = new GoogleGenerativeAI(ENV.GEMINI_API_KEY);
+const model = genAi.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export const CreateCourse = async(req, res)=>{
     try {
@@ -33,5 +39,87 @@ export const CreateCourse = async(req, res)=>{
         
     } catch (error) {
         console.log(`error from create course.${error}`)
+    }
+}
+
+export const getCourse = async(req, res)=>{
+    try {
+        const {search} = req.query; 
+        if(!search || !search.trim()===""){
+            const allCourses = await Course.find({})
+            return res.status(200).json(allCourses);
+        }
+
+        const prompt = `you are a intelligent assistant for a learning management 
+        platform system. A user is searching for courses. analyze the query and 
+        return the most relevant keyword from these catogeries. 
+
+        - Banking 
+        - Insurance
+        - Accounting
+        - Finance
+
+        only reply with one keyword that best matches the query no explanation 
+
+        user query: ${search}
+        `
+
+        const result = await model.generateContent(prompt);
+        const aiText = result?.response?.candidates?.[0]?.content?.parts?.[0]?.text?.trim()
+        .replace(/['"*+-]/g, "") || "";
+
+        const searchTerm = aiText || search;
+
+        const mongoQuery = {
+            $or :[
+                {title: {$regex: search, $options: "i"}},
+                {description: {$regex: search, $options: "i"}},
+            ]
+        };
+
+        const courses = await Course.find(mongoQuery).lean();
+
+
+        return res.status(200).json({
+            success:true,
+            courses,
+            searchTerm:search,
+            count:courses.length,
+        })
+
+
+        
+    } catch (error) {
+        console.log(`error from get courses.${error}`)
+    }
+}
+
+export const getsingleCourse = async(req, res)=>{
+    try {
+        const courseId = req.params.id;
+
+        const course = await Course.findById(courseId).populate("userId");
+
+        if(!course){
+            return res.status(404).json({
+                success:false,
+                message:"Course not found",
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            course,
+        })
+        
+    } catch (error) {
+        console.log(`error from getsingle course.${error}`)
+    }
+}
+
+export const getPurchasedCourse = async(req, res) =>{
+    try {
+        const courseId = req.params.id;
+    } catch (error) {
+        console.log(`error from get purchased course.${error}`)
     }
 }
