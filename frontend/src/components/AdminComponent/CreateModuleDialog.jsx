439d8d78a7bd71542d64ee89a5e73e1c2e723472
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-05-13 18:04:06 +0530

    change

---
+++ frontend/src/components/AdminComponent/CreateModuleDialog.jsx
@@ -0,0 +1,9 @@
+import react from "react-router-dom"
+
+const CreateModuleDialog = () => {
+
+}
+
+
+
+export default  CreateModuleDialog
---
+++ frontend/src/pages/Admin/module.jsx
@@ -0,0 +1,9 @@
+import react from "react-router-dom"
+
+const module = () => {
+
+}
+
+
+
+export default  module    
