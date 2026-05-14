6ac12aa1ed58c2f27511bb2d3422e9787d9c79e1
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-05-14 16:49:29 +0530

    change

--- frontend/src/components/AdminComponent/CreateModuleDialog.jsx
+++ frontend/src/components/AdminComponent/CreateModuleDialog.jsx
@@ -1,9 +1,11 @@
-import react from "react-router-dom"
+import react from "react-router-dom";
 
 const CreateModuleDialog = () => {
+  return (
+    <div className="p-8 max-w-4xl mx-auto">
+      <dialog></dialog>
+    </div>
+  );
+};
 
-}
-
-
-
-export default  CreateModuleDialog
+export default CreateModuleDialog;
--- frontend/src/pages/Admin/module.jsx
+++ frontend/src/pages/Admin/module.jsx
@@ -1,6 +1,12 @@
 import react from "react-router-dom"
+import CreateModuleDialog from "../../components/AdminComponent/CreateModuleDialog"
 
 const module = () => {
+    return (
+        <div className='p-8 max-w-4xl mx-auto'>
+            <CreateModuleDialog />
+        </div>
+    )
 
 }
 
