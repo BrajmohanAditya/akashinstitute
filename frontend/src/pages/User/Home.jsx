4e70c04d13d2b462bdd60c4fdddb16564bbb1204
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-06-02 16:06:36 +0530

    change

--- frontend/src/pages/User/Home.jsx
+++ frontend/src/pages/User/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CourseSection from "@/components/userComponent/courseSection";
 import HeroSection from "@/components/userComponent/heroSection";
+import FloatingWhatsApp from "@/components/ui/FloatingWhatsApp";
 
 const Home = () => {
   return (
     <div className="min-h-[88vh] bg-slate-50">
       <HeroSection />
       <CourseSection />
+       <FloatingWhatsApp />
     </div>
   );
 };
